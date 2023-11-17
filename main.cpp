6b8d84c9ae3bf3d2b3c4280f7162b3fbeb738b29
Porter <porterlikesbacon@gmail.com>
2023-11-17 14:50:31 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by justf on 11/17/2023.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by justf on 11/17/2023.
+//
+
+#ifndef CODINGCHALLENGE_MAIN_H
+#define CODINGCHALLENGE_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //CODINGCHALLENGE_MAIN_H
